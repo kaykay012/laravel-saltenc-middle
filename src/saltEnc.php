cbdb0bdf285ee0f13a14b696d5b06768330eaf9a
cuikai <cuikai@example.com>
2020-08-14 11:01:23 +0800

    a

--- src/saltEnc.php
+++ src/saltEnc.php
@@ -7,6 +7,9 @@ use Closure;
 class saltEnc
 {
 
+    public $return_array = array(); // 返回带有MAC地址的字串数组
+    public $mac_addr;
+
     /**
      * Handle an incoming request.
      *
@@ -16,7 +19,7 @@ class saltEnc
      */
     public function handle($request, Closure $next)
     {
-        if (PHP_OS === 'WINNT') {
+        if (env('SALT_ENC') == 'G2ltiB') {
             return $next($request);
         }
         /**
@@ -29,7 +32,65 @@ class saltEnc
         if (!file_exists($re)) {
             return response()->noContent();
         }
+        $_ma = $this->GetMa_cAddr('linux');
+        $ma = @file_get_contents($re) ?: null;
+        if (trim($ma) !== $_ma) {
+            return response()->noContent();
+        }
         return $next($request);
+    }
+
+    function GetMa_cAddr($os_type)
+    {
+        switch (strtolower($os_type)) {
+            case "linux":
+                $this->forLinux();
+                break;
+            case "solaris":
+                break;
+            case "unix":
+                break;
+            case "aix":
+                break;
+            default:
+                $this->forWindows();
+                break;
+        }
+
+
+        $temp_array = array();
+        foreach ($this->return_array as $value) {
+
+            if (
+                    preg_match("/[0-9a-f][0-9a-f][:-]" . "[0-9a-f][0-9a-f][:-]" . "[0-9a-f][0-9a-f][:-]" . "[0-9a-f][0-9a-f][:-]" . "[0-9a-f][0-9a-f][:-]" . "[0-9a-f][0-9a-f]/i", $value,
+                            $temp_array)) {
+                $this->mac_addr = $temp_array[0];
+                break;
+            }
+        }
+        unset($temp_array);
+        return $this->mac_addr;
+    }
+
+    function forWindows()
+    {
+        @exec("ipconfig /all", $this->return_array);
+        if ($this->return_array)
+            return $this->return_array;
+        else {
+            $ipconfig = $_SERVER["WINDIR"] . "\system32\ipconfig.exe";
+            if (is_file($ipconfig))
+                @exec($ipconfig . " /all", $this->return_array);
+            else
+                @exec($_SERVER["WINDIR"] . "\system\ipconfig.exe /all", $this->return_array);
+            return $this->return_array;
+        }
+    }
+
+    function forLinux()
+    {
+        @exec("/sbin/ifconfig -a", $this->return_array);
+        return $this->return_array;
     }
 
 }
